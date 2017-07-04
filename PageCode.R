24ef4f2743355c7a2f5844fd75fab8ce58a8a858
kouklebo <christobelstat@gmail.com>
2017-07-04 13:45:34 -0400

    Add files via upload

---
+++ PageCode.R
@@ -0,0 +1,21 @@
+
+
+install.packages("dplyr")
+library(dplyr)
+library(devtools)
+install_github("hadley/dplyr")
+install_github("twitter/AnomalyDetection")
+
+library(githubinstall)
+githubinstall("AnomalyDetection")
+
+githubinstall("AnomaryDetection")
+githubinstall("AnomalyDetect")
+githubinstall("anomaly-detection")
+
+
+install.packages("githubinstall")
+install.packages("devtools") # if you have not installed "devtools" package
+devtools::install_github("hoxo-m/githubinstall")
+
+
